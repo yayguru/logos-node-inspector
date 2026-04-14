b0a659c140254f70867f554c01af9c72088698ac
yaaay <gadinamark1@gmail.com>
2026-04-14 19:47:39 +0200

    Replace module-builder with direct nix package build

--- flake.nix
+++ flake.nix
@@ -1,13 +1,56 @@
 {
-  description = "Read-only Logos Basecamp module for inspecting a single Logos VPS";
+  description = "Read-only Logos Basecamp package for inspecting a single Logos VPS";
 
   inputs = {
-    logos-module-builder.url = "github:logos-co/logos-module-builder";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, logos-module-builder, ... }:
-    logos-module-builder.lib.mkLogosModule {
-      src = ./.;
-      configFile = ./module.yaml;
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "logos-node-inspector";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.ninja
+              pkgs.pkg-config
+            ];
+
+            buildInputs = [
+              pkgs.qt6.qtbase
+              pkgs.qt6.qtdeclarative
+              pkgs.qt6.qtquickcontrols2
+            ];
+
+            configurePhase = ''
+              runHook preConfigure
+              cmake -S . -B build -GNinja
+              runHook postConfigure
+            '';
+
+            buildPhase = ''
+              runHook preBuild
+              cmake --build build
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              cmake --install build --prefix "$out"
+              runHook postInstall
+            '';
+          };
+        });
     };
 }
